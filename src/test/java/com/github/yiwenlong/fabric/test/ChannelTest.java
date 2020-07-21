b7d5e825b7619fb0e208f28e9c9eace45132ca7f
yiwenlong <wlong.yi@gmail.com>
2020-07-22 07:10:31 +0800

    Add license for java files.

--- src/test/java/com/github/yiwenlong/fabric/test/ChannelTest.java
+++ src/test/java/com/github/yiwenlong/fabric/test/ChannelTest.java
@@ -1,3 +1,18 @@
+//
+// Copyright 2020 Yiwenlong(wlong.yi#gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package com.github.yiwenlong.fabric.test;
 
 import com.github.yiwenlong.fabric.network.SingleOrgNetwork.*;
@@ -57,7 +72,7 @@ public class ChannelTest {
         channel.addPeer(Org1.getPeer0Admin(client));
         channel.addOrderer(Orderers.getOrderer0(client));
         BlockchainInfo blockchainInfo = channel.initialize().queryBlockchainInfo();
-        
+
         System.out.println("Height: " + blockchainInfo.getHeight());
         System.out.println("Current block hash: " + Base64.getEncoder().encodeToString(blockchainInfo.getCurrentBlockHash()));
         System.out.println("Previous block hash: " + Base64.getEncoder().encodeToString(blockchainInfo.getPreviousBlockHash()));
