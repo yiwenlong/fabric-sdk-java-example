afd2ad2b8a6a802808d2085c0f4164e85b41fe67
yiwenlong <wlong.yi@gmail.com>
2020-07-25 23:30:48 +0800

    Refactor channel test cases.

--- src/test/java/com/github/yiwenlong/fabric/test/ChannelTest.java
+++ src/test/java/com/github/yiwenlong/fabric/test/ChannelTest.java
@@ -15,66 +15,33 @@
 //
 package com.github.yiwenlong.fabric.test;
 
-import com.github.yiwenlong.fabric.network.SingleOrgNetwork.*;
-import org.hyperledger.fabric.sdk.BlockchainInfo;
-import org.hyperledger.fabric.sdk.Channel;
-import org.hyperledger.fabric.sdk.HFClient;
-import org.hyperledger.fabric.sdk.exception.CryptoException;
-import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
-import org.hyperledger.fabric.sdk.security.CryptoSuite;
-import org.junit.Before;
+import junit.framework.TestResult;
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.lang.reflect.InvocationTargetException;
-import java.security.Security;
-import java.util.Base64;
 
 public class ChannelTest {
 
-    static {
-        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-    }
-
-    private HFClient client;
-
-    @Before
-    public void initClient() throws IllegalAccessException, InvocationTargetException, InvalidArgumentException, InstantiationException, NoSuchMethodException, CryptoException, ClassNotFoundException {
-        client = HFClient.createNewInstance();
-        client.setCryptoSuite(CryptoSuite.Factory.getCryptoSuite());
-        client.setUserContext(Org1.Admin.get());
+    @Test
+    public void testCreateChannel() {
+        TestResult r = new ChannelTestCase("createChannel").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 
     @Test
-    public void testCreateChannel() throws Exception {
-        Channel channel = MyChannel.create(client);
-//        channel.joinPeer(Org1.getPeer0Admin(client));
-//        channel.joinPeer(Org1.getPeer1Admin(client));
-        System.out.println(channel.getName());
+    public void testJoinChannelPeer0() {
+        TestResult r = new ChannelTestCase("joinChannelPeer0").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 
     @Test
-    public void testJoinChannelPeer0() throws Exception {
-        Channel channel = client.newChannel(MyChannel.name);
-        channel.addOrderer(Orderers.getOrderer0(client));
-        channel.joinPeer(Org1.getPeer0Admin(client));
+    public void testJoinChannelPeer1() {
+        TestResult r = new ChannelTestCase("joinChannelPeer1").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 
     @Test
-    public void testJoinChannelPeer1() throws Exception {
-        Channel channel = client.newChannel(MyChannel.name);
-        channel.addOrderer(Orderers.getOrderer0(client));
-        channel.joinPeer(Org1.getPeer1Admin(client));
-    }
-
-    @Test
-    public void testViewChannel() throws Exception {
-        Channel channel = client.newChannel(MyChannel.name);
-        channel.addPeer(Org1.getPeer0Admin(client));
-        channel.addOrderer(Orderers.getOrderer0(client));
-        BlockchainInfo blockchainInfo = channel.initialize().queryBlockchainInfo();
-
-        System.out.println("Height: " + blockchainInfo.getHeight());
-        System.out.println("Current block hash: " + Base64.getEncoder().encodeToString(blockchainInfo.getCurrentBlockHash()));
-        System.out.println("Previous block hash: " + Base64.getEncoder().encodeToString(blockchainInfo.getPreviousBlockHash()));
+    public void testViewChannel() {
+        TestResult r = new ChannelTestCase("viewChannelInformation").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 }
---
+++ src/test/java/com/github/yiwenlong/fabric/test/ChannelTestCase.java
@@ -0,0 +1,104 @@
+//
+// Copyright 2020 Yiwenlong(wlong.yi#gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.github.yiwenlong.fabric.test;
+
+import com.github.yiwenlong.fabric.network.SingleOrgNetwork;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.hyperledger.fabric.sdk.BlockchainInfo;
+import org.hyperledger.fabric.sdk.Channel;
+import org.hyperledger.fabric.sdk.HFClient;
+import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
+import org.hyperledger.fabric.sdk.exception.ProposalException;
+import org.hyperledger.fabric.sdk.exception.TransactionException;
+import org.hyperledger.fabric.sdk.security.CryptoSuite;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.security.Security;
+
+public class ChannelTestCase extends TestCase {
+
+    static {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+    }
+
+    private HFClient client;
+
+    public ChannelTestCase(String name) {
+        super(name);
+        client = HFClient.createNewInstance();
+        try {
+            client.setCryptoSuite(CryptoSuite.Factory.getCryptoSuite());
+            client.setUserContext(SingleOrgNetwork.Org1.Admin.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createChannel() throws IOException, InvalidArgumentException {
+        try {
+            Channel channel = SingleOrgNetwork.MyChannel.create(client);
+            Assert.assertEquals(channel.getName(), SingleOrgNetwork.MyChannel.name);
+        } catch (TransactionException ex) {
+            ex.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    public void joinChannelPeer0() throws InvalidArgumentException {
+        Channel channel = client.newChannel(SingleOrgNetwork.MyChannel.name);
+        channel.addOrderer(SingleOrgNetwork.Orderers.getOrderer0(client));
+        try {
+            channel.joinPeer(SingleOrgNetwork.Org1.getPeer0Admin(client));
+        } catch (ProposalException ex) {
+            Assert.fail();
+        }
+    }
+
+    public void joinChannelPeer1() throws InvalidArgumentException {
+        Channel channel = client.newChannel(SingleOrgNetwork.MyChannel.name);
+        channel.addOrderer(SingleOrgNetwork.Orderers.getOrderer0(client));
+        try {
+            channel.joinPeer(SingleOrgNetwork.Org1.getPeer1Admin(client));
+        } catch (ProposalException ex) {
+            Assert.fail();
+        }
+    }
+
+    public void viewChannelInformation() throws InvalidArgumentException {
+        Channel channel = client.newChannel(SingleOrgNetwork.MyChannel.name);
+        channel.addPeer(SingleOrgNetwork.Org1.getPeer0Admin(client));
+        channel.addOrderer(SingleOrgNetwork.Orderers.getOrderer0(client));
+        try {
+            BlockchainInfo blockchainInfo = channel.initialize().queryBlockchainInfo();
+            Assert.assertNotNull(blockchainInfo);
+        } catch (TransactionException | ProposalException ex) {
+            ex.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTest(new ChannelTestCase("createChannel"));
+        suite.addTest(new ChannelTestCase("joinChannelPeer0"));
+        suite.addTest(new ChannelTestCase("joinChannelPeer1"));
+        suite.addTest(new ChannelTestCase("viewChannelInformation"));
+        return suite;
+    }
+}
