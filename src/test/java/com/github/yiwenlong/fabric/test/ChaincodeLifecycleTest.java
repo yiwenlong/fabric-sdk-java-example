6731f42ec44405f763774a947f211b84d8a3966c
yiwenlong <wlong.yi@gmail.com>
2020-07-26 23:59:00 +0800

    Add ChaincodeLifecycleTestCase

--- src/test/java/com/github/yiwenlong/fabric/test/ChaincodeLifecycleTest.java
+++ src/test/java/com/github/yiwenlong/fabric/test/ChaincodeLifecycleTest.java
@@ -88,7 +88,6 @@ public class ChaincodeLifecycleTest {
                 e.printStackTrace();
             }
         });
-        client.newLifecycleQueryInstalledChaincodeRequest();
     }
 
     @Test
