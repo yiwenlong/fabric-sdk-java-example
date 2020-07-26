805d67c6e0d07c3efce0c12b7e72afe67b18a86b
yiwenlong <wlong.yi@gmail.com>
2020-07-27 00:14:51 +0800

    Modify ChaincodeLifecycleTest, using ChiancodeLifecycleTestCase instead.

--- src/test/java/com/github/yiwenlong/fabric/test/ChaincodeLifecycleTest.java
+++ src/test/java/com/github/yiwenlong/fabric/test/ChaincodeLifecycleTest.java
@@ -15,145 +15,45 @@
 //
 package com.github.yiwenlong.fabric.test;
 
-import com.github.yiwenlong.fabric.network.SingleOrgNetwork;
-import org.hyperledger.fabric.sdk.*;
-import org.hyperledger.fabric.sdk.exception.ProposalException;
-import org.hyperledger.fabric.sdk.security.CryptoSuite;
+import junit.framework.TestResult;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.File;
-import java.security.Security;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 
 public class ChaincodeLifecycleTest {
 
-    static {
-        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-    }
-
-    private HFClient client;
-    private Channel mychannel;
-    private Peer peer0;
-
-    @Before
-    public void initClient() throws Exception {
-        client = HFClient.createNewInstance();
-        client.setCryptoSuite(CryptoSuite.Factory.getCryptoSuite());
-        client.setUserContext(SingleOrgNetwork.Org1.Admin.get());
-
-        mychannel = client.newChannel(SingleOrgNetwork.MyChannel.name);
-        peer0 = SingleOrgNetwork.Org1.getPeer0Admin(client);
-        mychannel.addPeer(peer0);
-        mychannel.addOrderer(SingleOrgNetwork.Orderers.getOrderer0(client));
-        BlockchainInfo blockchainInfo = mychannel.initialize().queryBlockchainInfo();
-
-        System.out.println("Height: " + blockchainInfo.getHeight());
-        System.out.println("Current block hash: " + Base64.getEncoder().encodeToString(blockchainInfo.getCurrentBlockHash()));
-        System.out.println("Previous block hash: " + Base64.getEncoder().encodeToString(blockchainInfo.getPreviousBlockHash()));
-    }
-
-
     @Test
-    public void testInstallChaincode() throws Exception {
-          LifecycleInstallChaincodeRequest request = client.newLifecycleInstallChaincodeRequest();
-          request.setLifecycleChaincodePackage(LifecycleChaincodePackage.fromFile(new File(SingleOrgNetwork.TPS.chaincodePackage)));
-          Collection<LifecycleInstallChaincodeProposalResponse> responses = client.sendLifecycleInstallChaincodeRequest(request, SingleOrgNetwork.Org1.getPeersAdmin(client));
-
-          for (LifecycleInstallChaincodeProposalResponse response: responses) {
-              System.out.println("status: " + response.getStatus().name());
-              System.out.println("txid: " + response.getTransactionID());
-              System.out.println("message" + response.getMessage());
-              System.out.println("Package Id: " + response.getPackageId());
-          }
+    public void testInstallChaincode() {
+        TestResult r = new ChaincodeLifecycleTestCase("installChaincode").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 
     @Test
-    public void testQueryInstalledChaincode() throws Exception {
-        LifecycleQueryInstalledChaincodesRequest queryRequest = client.newLifecycleQueryInstalledChaincodesRequest();
-        Collection<LifecycleQueryInstalledChaincodesProposalResponse> responses = client.sendLifecycleQueryInstalledChaincodes(queryRequest, SingleOrgNetwork.Org1.getPeersAdmin(client));
-        responses.forEach( response -> {
-            try {
-                Collection<LifecycleQueryInstalledChaincodesProposalResponse.LifecycleQueryInstalledChaincodesResult> ress = response.getLifecycleQueryInstalledChaincodesResult();
-                ress.forEach( res -> {
-                    System.out.println(res.getLabel());
-                    System.out.println(res.getPackageId());
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+    public void testQueryInstalledChaincode() {
+        TestResult r = new ChaincodeLifecycleTestCase("queryInstalledChaincode").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 
     @Test
-    public void testApproveChaincode() throws Exception {
-        LifecycleApproveChaincodeDefinitionForMyOrgRequest request = client.newLifecycleApproveChaincodeDefinitionForMyOrgRequest();
-        request.setChaincodeName("tps");
-        request.setInitRequired(false);
-        request.setSequence(1);
-        request.setChaincodeVersion("1");
-        request.setPackageId("tps:818934162895283ba4fa6d04149aa40179768279d963d0c81f6b9feff20e4421");
-        Collection<Peer> peers = new ArrayList<>();
-        peers.add(peer0);
-        Collection<LifecycleApproveChaincodeDefinitionForMyOrgProposalResponse>  response = mychannel.sendLifecycleApproveChaincodeDefinitionForMyOrgProposal(request, peers);
-        CompletableFuture<BlockEvent.TransactionEvent> txFuture = mychannel.sendTransaction(response);
-        BlockEvent.TransactionEvent txEvent = txFuture.get();
-        Assert.assertTrue(txEvent.isValid());
+    public void testApproveChaincode() {
+        TestResult r = new ChaincodeLifecycleTestCase("approveChaincode").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 
     @Test
-    public void testQueyApproveChaincode() throws Exception {
-        Collection<Peer> peers = new ArrayList<>();
-        peers.add(peer0);
-        LifecycleCheckCommitReadinessRequest request = client.newLifecycleSimulateCommitChaincodeDefinitionRequest();
-        request.setChaincodeName("tps");
-        request.setInitRequired(false);
-        request.setSequence(1);
-        request.setChaincodeVersion("1");
-        Collection<LifecycleCheckCommitReadinessProposalResponse> responses  = mychannel.sendLifecycleCheckCommitReadinessRequest(request, peers);
-        responses.forEach(response -> {
-            try {
-                Map<String, Boolean> approvalsMap = response.getApprovalsMap();
-                approvalsMap.forEach((org, isProval) -> System.out.printf("Org: %s, %s\n", org, isProval));
-            } catch (ProposalException e) {
-                e.printStackTrace();
-            }
-        });
+    public void testQueyApproveChaincode() {
+        TestResult r = new ChaincodeLifecycleTestCase("queryApproveChaincode").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 
     @Test
-    public void testCommitChaincode() throws Exception {
-        LifecycleCommitChaincodeDefinitionRequest request = client.newLifecycleCommitChaincodeDefinitionRequest();
-        request.setChaincodeName("tps");
-        request.setSequence(1);
-        request.setChaincodeVersion("1");
-        request.setInitRequired(false);
-        Collection<Peer> peers = new ArrayList<>();
-        peers.add(peer0);
-        Collection<LifecycleCommitChaincodeDefinitionProposalResponse> responses = mychannel.sendLifecycleCommitChaincodeDefinitionProposal(request, peers);
-        responses.forEach(response -> System.out.println(response.getMessage()));
-        CompletableFuture<BlockEvent.TransactionEvent> txFuture = mychannel.sendTransaction(responses);
-        BlockEvent.TransactionEvent txEvent = txFuture.get();
-        Assert.assertTrue(txEvent.isValid());
+    public void testCommitChaincode() {
+        TestResult r = new ChaincodeLifecycleTestCase("commitChaincode").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 
     @Test
-    public void testChaincodeDefinition() throws Exception {
-        LifecycleQueryChaincodeDefinitionsRequest request = client.newLifecycleQueryChaincodeDefinitionsRequest();
-        Collection<Peer> peers = new ArrayList<>();
-        peers.add(peer0);
-        Collection<LifecycleQueryChaincodeDefinitionsProposalResponse> reponses = mychannel.lifecycleQueryChaincodeDefinitions(request, peers);
-        reponses.forEach(response -> {
-            try {
-                response.getLifecycleQueryChaincodeDefinitionsResult().iterator().forEachRemaining(result -> System.out.println(result.getName()));
-            } catch (ProposalException e) {
-                e.printStackTrace();
-            }
-        });
+    public void testChaincodeDefinition() {
+        TestResult r = new ChaincodeLifecycleTestCase("queryCommittedChaincode").run();
+        Assert.assertTrue(r.wasSuccessful());
     }
 }
