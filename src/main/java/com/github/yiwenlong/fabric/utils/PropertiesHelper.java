8cb405fa696f6b711632240a41bd338781b9ba6a
yiwenlong <wlong.yi@gmail.com>
2020-07-30 23:36:43 +0800

    Refactor code: PropertiesHelper.createTlsAccessProperties.

---
+++ src/main/java/com/github/yiwenlong/fabric/utils/PropertiesHelper.java
@@ -0,0 +1,23 @@
+package com.github.yiwenlong.fabric.utils;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class PropertiesHelper {
+
+    public static Properties createTlsAccessProperties(String tlscaFilePath) {
+        Properties properties = new Properties();
+        properties.setProperty("sslProvider", "openSSL");
+        properties.setProperty("negotiationType", "TLS");
+        properties.put("pemFile", tlscaFilePath);
+
+        // for client auth tls.
+        // properties.put("clientKeyFile", clientTlsKey);
+        // properties.put("clientCertFile",clientTlsCert);
+
+        properties.put("grpc.NettyChannelBuilderOption.keepAliveTime", new Object[] {5L, TimeUnit.MINUTES});
+        properties.put("grpc.NettyChannelBuilderOption.keepAliveTimeout", new Object[] {8L, TimeUnit.SECONDS});
+        properties.put("grpc.NettyChannelBuilderOption.keepAliveWithoutCalls", new Object[] {true});
+        return properties;
+    }
+}
